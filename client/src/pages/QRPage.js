724d1901f99c610eae782945a651900e37539c7f
MikeForde <mikeforde@ymail.com>
2024-05-04 13:45:22 +0100

    Deployment 0_3

--- client/src/pages/QRPage.js
+++ client/src/pages/QRPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+//import { useParams } from 'react-router-dom';
 import QRCode from 'qrcode.react';
 import axios from 'axios';
 import './HomePage.css'; 
